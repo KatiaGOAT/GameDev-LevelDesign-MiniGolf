41c0fbbea83aeb1e8bf40d53e17aff16a9d2bbf3
KatiaGOAT <147525378+KatiaGOAT@users.noreply.github.com>
2024-11-21 18:37:24 +0000

    populated the scene with extra assets

--- Assets/Scripts/ExitScript.cs
+++ Assets/Scripts/ExitScript.cs
@@ -7,9 +7,12 @@ public class ExitScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Entered the ontrigger event");
+
         // Check if the collision is with a specific object or tag
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Changing the scene now");
             SceneManager.LoadScene("LevelDesign_Scene");
         }
     }
